62ed7661ac3494bbe1a594040ace6a0e426cc108
janarthanan <janarthanan@yujdesigns.com>
2024-04-09 15:25:17 +0530

    Initial Commit

---
+++ Charts-SwiftUI/Charts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_SwiftUIApp.swift
+//  Charts-SwiftUI
+//
+//  Created by DB-MBP-023 on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Charts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Charts-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Charts-SwiftUI
+//
+//  Created by DB-MBP-023 on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
